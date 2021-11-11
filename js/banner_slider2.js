9467e782e953e7f3f4d67114fa661c2f6ad03496
dudwnsbook <83794116+dudwnsbook@users.noreply.github.com>
2021-11-12 00:28:45 +0900

    ex
    ex

--- js/banner_slider2.js
+++ js/banner_slider2.js
@@ -1,10 +1,11 @@
-
-
-var slideWrap=document.getElementsByClassName('banner');
+var slideWrap = document.getElementsByClassName('banner');
+var slideUl = document.getElementsByClassName("banner_li");
 var slideCou = document.getElementsByClassName('slideCon');
-console.log(slideCou);
+
 
 var bannLen = slideCou.length;
+var leftBtn = document.querySelector(".leftBtn");
+var rightBtn = document.querySelector(".rightBtn");
 var count=0;
 var currentIndex;
 var prevIndex;
@@ -22,15 +23,41 @@ var btnLen = btn.length;
 //    }
 //    count=obj2;
 //}
+
 function FadeSlider(obj) {
-    slideCou[obj].classList.add('ViewImg');
-    prevIndex = count % bannLen;
-    slideCou[prevIndex].classList.remove( 'ViewImg');
+    slideUl[0].style.left=(-100 * obj) +"%";
+    slideUl[0].style.transition="1s";
+    if(obj==4){
+        slideUl[0].style.transition="0.1s";
+    }
+    for(var b = 0; b<btnLen; b++){
+        if(b==obj){
+            btn[obj].classList.add("on");
+        }
+        else{
+            btn[b].classList.remove("on");
+        }
+    }
+    
+    // slideCou[obj].classList.add('ViewImg');
+    // prevIndex = count % bannLen;
+    // slideCou[prevIndex].classList.remove( 'ViewImg');
+    
     count=obj;
 
 //    autoSlide(count);
 }
-
+for (var a = 0; a<btnLen; a++){
+    btn[a].addEventListener('click',function(e){
+        
+        
+        currentIndex=e.target.getAttribute("data-value")-1;
+        // clearInterval(timeSlide); 
+        console.log(currentIndex);
+        FadeSlider(currentIndex);
+        
+    })
+ }
 function autoSlide() {
     timeSlide =setInterval(function () {
         currentIndex = (count+1) % bannLen;
@@ -42,23 +69,33 @@ function autoSlide() {
     }, 4000);
 }
 
+leftBtn.addEventListener("click",function(){
+    if (currentIndex > 0) {
+        currentIndex=currentIndex-1;
+        
+        
+    } else {
+        currentIndex=bannLen-1;
+        
+    }
+    FadeSlider(currentIndex);
+})
+rightBtn.addEventListener("click",function(){
+    if(currentIndex<3){
+        currentIndex=currentIndex+1;
+    }
+    else{
+        currentIndex=0;
+    }
+    FadeSlider(currentIndex);
+})
 
 
-//for (var a = 0; a<btnLen; a++){
-//    btn[a].addEventListener('click',function(e){
-//        
-//        
-//        var btnIndex=e.target.innerText;
-//
-//        console.log(btnIndex);
-//        FadeSlider(btnIndex);
-//        
-//    })
-//}
 autoSlide();
 
 slideWrap[0].addEventListener('mouseenter', function () {
     clearInterval(timeSlide); 
+    console.log("??")
 });
 slideWrap[0].addEventListener('mouseleave', function () {
     autoSlide();
